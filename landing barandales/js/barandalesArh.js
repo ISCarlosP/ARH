efd27fa7f09708b5bd95781759d3e28c018c2fd1
SaehCls <carlosp.nu22@gmail.com>
2023-07-13 23:15:39 -0600

    Update
    Se agrego loader y redistribucion del contenido

--- landing barandales/js/barandalesArh.js
+++ landing barandales/js/barandalesArh.js
@@ -1,3 +1,10 @@
 toggleLoginModal = function(){
     $('#loginModal').modal('show');
 }
+
+toggleLoader = function(){
+    setTimeout(function(){
+        document.getElementById('loader').classList.add('d-none');
+        document.getElementById('app').classList.remove('d-none');
+    }, 2000);
+}
