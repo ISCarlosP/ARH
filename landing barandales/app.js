dad4fb80c7d8dae3578c3f147d5f398355329b67
Steven96lg <steven96_layna@hotmail.com>
2023-07-16 14:32:01 -0700

    footer corregido y redes sociales agregadas

--- landing barandales/app.js
+++ landing barandales/app.js
@@ -1,4 +1,43 @@
 
+
+const floatNav = document.querySelector('#floatNav')
+
+floatNav.children[0].addEventListener('mouseover', () => {
+  floatNav.children[0].children[1].style.display = 'block'
+  floatNav.children[1].style.width = '50px'
+  floatNav.children[2].style.width = '50px'
+})
+
+floatNav.children[0].addEventListener('mouseout', () => {
+  floatNav.children[0].children[1].style.display = 'none'
+  floatNav.children[1].style.width = 'auto'
+  floatNav.children[2].style.width = 'auto'
+})
+
+floatNav.children[1].addEventListener('mouseover', () => {
+  floatNav.children[0].style.width = '50px'
+  floatNav.children[1].children[1].style.display = 'block'
+  floatNav.children[2].style.width = '50px'
+})
+
+floatNav.children[1].addEventListener('mouseout', () => {
+  floatNav.children[0].style.width = 'auto'
+  floatNav.children[1].children[1].style.display = 'none'
+  floatNav.children[2].style.width = 'auto'
+})
+
+floatNav.children[2].addEventListener('mouseover', () => {
+  floatNav.children[0].style.width = '50px'
+  floatNav.children[1].style.width = '50px'
+  floatNav.children[2].children[1].style.display = 'block'
+})
+
+floatNav.children[2].addEventListener('mouseout', () => {
+  floatNav.children[0].style.width = 'auto'
+  floatNav.children[1].style.width = 'auto'
+  floatNav.children[2].children[1].style.display = 'none'
+})
+
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'vertical',
@@ -19,4 +58,4 @@ const swiper = new Swiper('.swiper', {
     scrollbar: {
       el: '.swiper-scrollbar',
     },
-  });
+});
