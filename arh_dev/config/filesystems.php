42cac9af0c7c1a65ced8e6f8f36f4d88b3a8a32a
SaehCls <carlosp.nu22@gmail.com>
2023-08-19 23:17:35 -0600

    asasas
    asasas

--- arh_dev/config/filesystems.php
+++ arh_dev/config/filesystems.php
@@ -38,8 +38,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('img'),
-            'url' => env('APP_URL').'/public',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
